e7283a5da76d498a8f2015bd9f165d26e3be5cce
Praveen D <praveen@hasura.io>
2017-11-28 13:12:25 +0530

    add public folder

--- src/Home.js
+++ src/Home.js
@@ -5,14 +5,7 @@ class Home extends React.Component {
     return (
       <div>
         <h2>HELLO</h2>
-        <p>Cras facilisis urna ornare ex volutpat, et
-        convallis erat elementum. Ut aliquam, ipsum vitae
-        gravida suscipit, metus dui bibendum est, eget rhoncus nibh
-        metus nec massa. Maecenas hendrerit laoreet augue
-        nec molestie. Cum sociis natoque penatibus et magnis
-        dis parturient montes, nascetur ridiculus mus.</p>
- 
-        <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
+        <div>Landing page with Signin Methods</div>
       </div>
     );
   }
