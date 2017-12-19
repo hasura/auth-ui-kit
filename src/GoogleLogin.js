429161cf7a9c1385364ffa83aa61387672738d68
Praveen D <praveen@hasura.io>
2017-12-19 20:43:20 +0530

    social login flow complete

---
+++ src/GoogleLogin.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import {Helmet} from "react-helmet";
+import {googleLogin} from './api';
+import getParams from './utils/getParams';
+import globals from './globals';
+import './style.css';
+
+class GoogleLogin extends Component {
+
+  componentWillMount() {
+    let locationParams = window.location.search;
+    if(locationParams === "") {
+      locationParams = window.location.hash;
+    }
+    const params = getParams(locationParams);
+    const id_token = params.id_token;
+    const redirectUrl = params.state;
+    // check for error before proceeding
+    const error = params.error;
+    if (error) {
+      alert(error);
+    } else {
+      googleLogin(id_token, redirectUrl);
+    }
+  }
+
+  render() {
+
+    const pageWrapperThemeClass = globals.theme === 'light' ? 'LightLandingPageWrapper' : 'DarkLandingPageWrapper';
+    const pageInnerThemeClass = globals.theme === 'light' ? 'LightLandingPageInnerWrapper' : 'DarkLandingPageInnerWrapper';
+  
+    return (
+      <div className={'landingPageWrapper container-fluid ' + pageWrapperThemeClass}>
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>Google Login</title>
+        </Helmet>
+        <div className={'landingPageInnerWrapper ' + pageInnerThemeClass}>
+          <div className='signUpWrapper'>
+            <div className='headerDescription'>
+              Proceeding to Login...Will redirect after login
+            </div>
+            <div className='descriptionText'>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default GoogleLogin
---
+++ src/LinkedinLogin.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import {Helmet} from "react-helmet";
+import {linkedinLogin} from './api';
+import getParams from './utils/getParams';
+import globals from './globals';
+import './style.css';
+
+class LinkedinLogin extends Component {
+
+  componentWillMount() {
+    let locationParams = window.location.search;
+    if(locationParams === "") {
+      locationParams = window.location.hash;
+    }
+    const params = getParams(locationParams);
+    const code = params.code;
+    const redirectUrl = params.state;
+    // check for error before proceeding
+    const error = params.error;
+    const errorMessage = params.error_description;
+    if (error) {
+      alert(errorMessage);
+    } else {
+      linkedinLogin(code, redirectUrl);
+    }
+  }
+
+  render() {
+
+    const pageWrapperThemeClass = globals.theme === 'light' ? 'LightLandingPageWrapper' : 'DarkLandingPageWrapper';
+    const pageInnerThemeClass = globals.theme === 'light' ? 'LightLandingPageInnerWrapper' : 'DarkLandingPageInnerWrapper';
+  
+    return (
+      <div className={'landingPageWrapper container-fluid ' + pageWrapperThemeClass}>
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>Linkedin Login</title>
+        </Helmet>
+        <div className={'landingPageInnerWrapper ' + pageInnerThemeClass}>
+          <div className='signUpWrapper'>
+            <div className='headerDescription'>
+              Proceeding to Login...Will redirect after login
+            </div>
+            <div className='descriptionText'>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default LinkedinLogin
